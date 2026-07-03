34b8d6b1da3e04957aa1f934f623cb2a819583be
Jono-byte <ferdithamsis@gmail.com>
2026-07-03 19:10:59 +0700

    Update stop.js

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -1,1 +1,16 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("stop")
+    .setDescription("Stop music"),
+
+  async execute(interaction, client) {
+    const player = client.manager.getPlayer(interaction.guild.id);
+
+    if (!player) return interaction.reply("❌ No music");
+
+    player.destroy();
+    interaction.reply("⛔ Stopped");
+  }
+};
