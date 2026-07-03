482d46fb903432a70f0da33fe5a0d518b1dc4095
Jono-byte <ferdithamsis@gmail.com>
2026-07-03 19:10:44 +0700

    Update skip.js

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -1,1 +1,16 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("skip")
+    .setDescription("Skip music"),
+
+  async execute(interaction, client) {
+    const player = client.manager.getPlayer(interaction.guild.id);
+
+    if (!player) return interaction.reply("❌ No music");
+
+    player.skip();
+    interaction.reply("⏭ Skipped");
+  }
+};
