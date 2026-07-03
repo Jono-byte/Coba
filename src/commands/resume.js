7df52af27aefeeef5af15391638c46b84bd96625
Jono-byte <ferdithamsis@gmail.com>
2026-07-03 19:12:17 +0700

    Update resume.js

--- src/commands/resume.js
+++ src/commands/resume.js
@@ -1,1 +1,15 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("pause")
+    .setDescription("Pause music"),
+
+  async execute(interaction, client) {
+    const player = client.manager.getPlayer(interaction.guild.id);
+    if (!player) return interaction.reply("❌ No music");
+
+    player.pause(true);
+    interaction.reply("⏸ Paused");
+  }
+};
